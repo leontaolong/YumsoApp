031fe04e6d6ba34f8ac574982681b686087bf834
Yao <yao@Yaos-MacBook-Pro.local>
2016-04-23 03:41:55 -0700

    chefCommentsPage UI prototype done

--- YumsoApp/chefCommentsPage.js
+++ YumsoApp/chefCommentsPage.js
@@ -2,6 +2,12 @@ var HttpsClient = require('./httpsClient');
 var styles = require('./style');
 var config = require('./config');
 var AuthService = require('./authService');
+var rating = require('./rating');
+var dateRender = require('./commonModules/dateRender');
+import Dimensions from 'Dimensions';
+
+var windowHeight = Dimensions.get('window').height;
+var windowWidth = Dimensions.get('window').width;
 
 import React, {
   Component,
@@ -63,46 +69,226 @@ class ChefCommentsPage extends Component {
                 size="large"
                 style={styles.loader}/> 
         }   
+        // return (
+        //     <View style={styles.dishListView_dish}>
+        //         <Image source={imageSrc} style={styles.dishListView_dish_pic}/>
+        //         <View >
+        //             <View>
+        //                 <Text> {comment.eaterAlias}</Text>
+        //                 <Text> {comment.eaterComment==undefined?'no comment':comment.eaterComment}</Text>
+        //                 <Text> rating: {comment.starRating}</Text>
+        //                 <Text>{comment.eaterCommentTime==undefined? '':this.parseTime(comment.eaterCommentTime)}</Text>     
+        //                 <Text>Chef reply:</Text>
+        //                 <Text>{comment.chefComment==undefined? 'no reply':comment.chefComment}</Text>
+        //                 <Text>{comment.chefCommentTime==undefined? '':this.parseTime(comment.chefCommentTime)}</Text>
+        //             </View>                          
+        //         </View>           
+        //     </View>
+        // );
+        
         return (
-            <View style={styles.dishListView_dish}>
-                <Image source={imageSrc} style={styles.dishListView_dish_pic}/>
-                <View >
-                    <View>
-                        <Text> {comment.eaterAlias}</Text>
-                        <Text> {comment.eaterComment==undefined?'no comment':comment.eaterComment}</Text>
-                        <Text> rating: {comment.starRating}</Text>
-                        <Text>{comment.eaterCommentTime==undefined? '':this.parseTime(comment.eaterCommentTime)}</Text>     
-                        <Text>Chef reply:</Text>
-                        <Text>{comment.chefComment==undefined? 'no reply':comment.chefComment}</Text>
-                        <Text>{comment.chefCommentTime==undefined? '':this.parseTime(comment.chefCommentTime)}</Text>
-                    </View>                          
-                </View>           
+            <View style={styleChefCommentsPage.oneListingView}>
+                <View style={styleChefCommentsPage.eaterPhotoView}>
+                   <Image source={imageSrc} style={styleChefCommentsPage.eaterPhoto}/>
+                </View>
+                <View style={styleChefCommentsPage.eaterChefCommetView}>
+                   <View style={styleChefCommentsPage.eaterNameRatingView}>
+                      <View style={styleChefCommentsPage.eaterNameView}>
+                          <Text style={styleChefCommentsPage.eaterNameText}>{comment.eaterAlias}</Text>
+                      </View>
+                      <View style={styleChefCommentsPage.orderRatingView}>
+                          {rating.renderRating(comment.starRating)}
+                      </View>
+                   </View>
+                   
+                   <View style={styleChefCommentsPage.eaterCommentView}>
+                        <Text style={styleChefCommentsPage.commentText}>{comment.eaterComment==undefined?'No Comment':comment.eaterComment}</Text>
+                        <View style={styleChefCommentsPage.commentTimeView}>
+                            <Text style={styleChefCommentsPage.commentTimeText}>{comment.eaterCommentTime==undefined? '':dateRender.renderDate1(comment.eaterCommentTime)}</Text>
+                        </View>
+                   </View>
+                   {this.displayChefComment(comment)}                         
+                </View>
+                                
             </View>
         );
     }
+    
+    displayChefComment(comment){
+         if(comment.chefComment){
+            var chefCommentSection=(<View style={styleChefCommentsPage.chefCommentView}>
+                                      <View style={styleChefCommentsPage.CHEFView}>
+                                         <Text style={styleChefCommentsPage.CHEFText}>CHEF</Text>
+                                      </View>
+                                      <Text style={styleChefCommentsPage.commentText}>{comment.chefComment}</Text>                                  
+                                      <View style={styleChefCommentsPage.commentTimeView}>
+                                         <Text style={styleChefCommentsPage.commentTimeText}>{comment.eaterCommentTime==undefined? '':dateRender.renderDate1(comment.chefCommentTime)}</Text>
+                                      </View>
+                                    </View>);
+           return chefCommentSection;
+        }
+        return[];
+   }
     
     render() {
         return (
             <View style={styles.container}>
-               <Text>Chef's ratig & comments</Text>
+               <View style={styleChefCommentsPage.headerBannerView}>    
+                   <View style={styleChefCommentsPage.backButtonView}>
+                   <TouchableHighlight onPress={() => this.navigateBackToChefPage()}>
+                     <Image source={require('./icons/ic_keyboard_arrow_left_48pt_3x.png')} style={styleChefCommentsPage.backButtonIcon}/>
+                   </TouchableHighlight>
+                   </View>    
+                   <View style={styleChefCommentsPage.historyOrderTitleView}>
+                     <Text style={styleChefCommentsPage.historyOrderTitleText}>REVIEW</Text>
+                   </View>
+                   <View style={{flex:0.1/3,width:windowWidth/3}}>
+                   </View>
+               </View>
                <ListView style={styles.dishListView}
                     dataSource = {this.state.dataSource}
-                    renderRow={this.renderRow.bind(this) } />
-                <TouchableHighlight style={styles.button}
-                    onPress={() => this.navigateBackToChefPage() }>
-                    <Text style={styles.buttonText}>back to dish list</Text>
-                </TouchableHighlight>                      
+                    renderRow={this.renderRow.bind(this) }/>                  
             </View>
         );
     }
 
-    parseTime(timeNumber){
-       return new Date(timeNumber).getDate()+'/'+new Date(timeNumber).getMonth()+'/'+new Date(timeNumber).getFullYear(); 
-    }
-    
     navigateBackToChefPage() {
         this.props.navigator.pop();
     }
 }
+
+var styleChefCommentsPage = StyleSheet.create({
+    headerBannerView:{
+        flex:0.1,
+        flexDirection:'row',
+        borderBottomWidth:1,
+        borderColor:'#D7D7D7',
+    },
+    backButtonView:{
+        flex:0.1/3,
+        width:windowWidth/3,
+        paddingTop:6,
+    },
+    backButtonIcon:{
+        width:30,
+        height:30,
+    },
+    historyOrderTitleView:{
+        flex:0.1/3, 
+        width:windowWidth/3,
+        alignItems:'center',     
+    },
+    historyOrderTitleText:{
+        marginTop:12,
+    },
+    commentListView:{
+        alignSelf:'stretch',
+        flexDirection:'column',
+        height: windowHeight*9/10
+    },
+    oneListingView:{
+       flex:1,
+       flexDirection:'row',
+       paddingHorizontal:10,
+       paddingVertical:20,
+       borderBottomWidth:1,
+       borderColor: '#f5f5f5',
+    },
+    eaterChefCommetView:{
+        flex:1,
+        flexDirection:'column',
+        marginLeft:10,
+    },
+    shopNameTimePriceView:{
+        flex:1,
+        flexDirection:'row',
+        marginBottom:7,
+    },
+    eaterNameRatingView:{
+        flex:1,
+        flexDirection:'row',
+    },
+    eaterNameView:{
+        flex:0.75,
+        flexDirection:'row',
+    },
+    eaterNameText:{
+        fontSize:15,
+        fontWeight:'600',
+    },
+    orderRatingView:{
+        flex:0.25,
+        flexDirection:'row',
+        marginBottom:10,
+    },
+    eaterCommentView:{
+        flex:1,
+        paddingHorizontal:10,
+        paddingVertical:6,
+        borderRadius: 6, 
+        borderWidth: 0, 
+        backgroundColor: '#f5f5f5',
+        overflow: 'hidden', 
+        marginBottom:10,
+    },
+    eaterPhotoView:{
+        borderRadius: 8, 
+        borderWidth: 0, 
+        overflow: 'hidden', 
+    },
+    eaterPhoto:{
+        width:windowHeight/13.8,
+        height:windowHeight/13.8,
+    },
+    CHEFView:{
+        backgroundColor:'#FF4500',
+        width:55,
+        paddingLeft:12,
+        marginVertical:5,
+    },
+    CHEFText:{
+        fontSize:13,
+        color:'#fff',
+    },
+    chefCommentView:{
+        flex:1,
+        flexDirection:'column',
+        backgroundColor: '#DCDCDC',
+        paddingRight:12,
+        paddingVertical:6,
+        borderRadius: 6, 
+        borderWidth: 0, 
+        overflow: 'hidden', 
+    },
+    commentText:{
+        fontSize:12,
+        color:'#696969',
+        marginBottom:5,
+        marginLeft:12,
+    },
+    commentTimeView:{
+        flex:1,
+        flexDirection:'row',
+        justifyContent:'flex-end',
+    },
+    commentTimeText:{
+        fontSize:12,
+        color:'#696969',
+    },
+    eaterNoCommentView:{
+        flex:1,
+        flexDirection:'row',
+        paddingHorizontal:10,
+        paddingVertical:6,
+        borderRadius: 6, 
+        borderWidth: 0, 
+        backgroundColor: '#f5f5f5',
+        overflow: 'hidden', 
+    },
+    addCommentTextClickable:{
+        color:'#ff9933',
+        fontSize:13,
+    }
+}); 
 
 module.exports = ChefCommentsPage;
