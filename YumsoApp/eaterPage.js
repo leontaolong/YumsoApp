bebd902c679f4cc2c053695ff0f63e090ba9c5ce
Steve Kellen <stevekellen@Steves-MacBook-Pro.local>
2016-04-23 14:35:50 -0700

    add eater page edit

--- YumsoApp/eaterPage.js
+++ YumsoApp/eaterPage.js
@@ -14,7 +14,8 @@ import React, {
   TouchableHighlight,
   ActivityIndicatorIOS,
   AsyncStorage,
-  Navigator
+  Alert,
+  TextInput,
 } from 'react-native';
 
 var windowHeight = Dimensions.get('window').height;
@@ -26,75 +27,162 @@ class EaterPage extends Component {
         var routeStack = this.props.navigator.state.routeStack;
         let eater = routeStack[routeStack.length-1].passProps.eater;      
         let principal = routeStack[routeStack.length-1].passProps.eater;
+        let callback = routeStack[routeStack.length-1].passProps.callback;
         this.state = {
             eater:eater,
             authProvider:principal.identityProvider,
-            showProgress:false
+            showProgress:false,
+            edit:false,
+            callback:callback
         };
-        this.client = new HttpsClient(config.baseUrl);
+        this.client = new HttpsClient(config.baseUrl, true);
     }
     
-    render() {
-            if(this.state.showProgress){
-                return (
-                <View>
-                    <ActivityIndicatorIOS
-                        animating={this.state.showProgress}
-                        size="large"
-                        style={styles.loader}/>
-                </View>);
-            }else{
-              var addressListRendered=[];
-              for(let i=0;i<this.state.eater.addressList.length;i++){
-                  addressListRendered.push(
-                    <Text key={i} style={styleEaterPage.eaterPageGreyText}>+ {this.state.eater.addressList[i]}</Text>  
-                  );
-              }
-              var chefProfile = this.state.eater.eaterProfilePic==null? require('./TestImages/Obama.jpg'):{uri: this.state.eater.eaterProfilePic}
-              return(
-              <View>
-                <View style={styleEaterPage.headerBannerView}>
-                <TouchableHighlight onPress={() => this.navigateBackToChefList()}>
-                   <View style={styleEaterPage.backButtonView}>
-                     <Image source={require('./icons/ic_keyboard_arrow_left_48pt_3x.png')} style={styleEaterPage.iconImage}/>
-                   </View>
-                </TouchableHighlight>
-                <TouchableHighlight>
-                   <View style={styleEaterPage.editButtonView}>
-                     <Text style={styleEaterPage.editButtonText}>Edit</Text>
-                   </View>
-                </TouchableHighlight>
-                </View>
-                <Image source={chefProfile} style={styleEaterPage.eaterProfilePic}>
-                   <View style={styleEaterPage.uploadPhotoButtonView}>
-                     <TouchableHighlight onPress={()=>this.uploadPic()}>
-                        <Image source={require('./icons/ic_add_a_photo_48pt_3x.png')} style={styleEaterPage.iconImage}/>                      
+     render() {
+         if (this.state.edit) {
+             return (
+                 <View style={styles.container}>
+                     <TextInput defaultValue={this.state.eater.firstname} style={styles.loginInput}
+                         onChangeText = {(text) => this.setState({ firstname: text }) }/>
+                     <TextInput defaultValue={this.state.eater.lastname} style={styles.loginInput}
+                         onChangeText = {(text) => this.setState({ lastname: text }) }/>
+                     <TextInput defaultValue={this.state.eater.eaterAlias} style={styles.loginInput}
+                         onChangeText = {(text) => this.setState({ eaterAlias: text }) }/>
+                     <TouchableHighlight style={styles.button}
+                         onPress = {this.submit.bind(this) }>
+                         <Text style={styles.buttonText}>Submit</Text>
                      </TouchableHighlight>
-                   </View>
-                </Image>
-                <View style={styleEaterPage.eaterPageRowView}>
-                   <Text style={styleEaterPage.eaterNameText}>{this.state.eater.firstname} {this.state.eater.lastname} ({this.state.eater.eaterAlias})</Text>
-                   <Text style={styleEaterPage.eaterPageGreyText}>{this.state.authProvider==='Yumso'?`Email:${this.state.eater.email}`:'Logged in using Facebook'}</Text>
-                </View>
-                <View style={styleEaterPage.eaterPageRowView}>
-                   <Text style={styleEaterPage.eaterPageGreyText}>Address:</Text>
-                   {addressListRendered}
-                </View>
-                <View style={styleEaterPage.eaterPageRowView}>
-                   <Text style={styleEaterPage.eaterPageGreyText}>Payment:</Text>
-                   <Text style={styleEaterPage.eaterPageGreyText}>+ Credit Card: xxxx xxxx xxxx 1234</Text>
-                    <Text style={styleEaterPage.eaterPageGreyText}>+ Paypal:</Text>
-                </View> 
-              </View>
-              );     
-            }                      
-    }
+                     <TouchableHighlight style={styles.button}
+                         onPress = {() => { this.setState({ edit: false }) } }>
+                         <Text style={styles.buttonText}>Cancel</Text>
+                     </TouchableHighlight>
+                     <ActivityIndicatorIOS
+                         animating={this.state.showProgress}
+                         size="large"
+                         style={styles.loader} />
+                 </View>);
+         }
+         if (this.state.showProgress) {
+             return (
+                 <View>
+                     <ActivityIndicatorIOS
+                         animating={this.state.showProgress}
+                         size="large"
+                         style={styles.loader}/>
+                 </View>);
+         } else {
+             var addressListRendered = [];
+             for (let i = 0; i < this.state.eater.addressList.length; i++) {
+                 addressListRendered.push(
+                     <Text key={i} style={styleEaterPage.eaterPageGreyText}>+ {this.state.eater.addressList[i]}</Text>
+                 );
+             }
+             var chefProfile = this.state.eater.eaterProfilePic == null ? require('./TestImages/Obama.jpg') : { uri: this.state.eater.eaterProfilePic }
+             return (
+                 <View>
+                     <View style={styleEaterPage.headerBannerView}>
+                         <TouchableHighlight onPress={() => this.navigateBackToChefList() }>
+                             <View style={styleEaterPage.backButtonView}>
+                                 <Image source={require('./icons/ic_keyboard_arrow_left_48pt_3x.png') } style={styleEaterPage.iconImage}/>
+                             </View>
+                         </TouchableHighlight>
+                         <TouchableHighlight>
+                             <View style={styleEaterPage.editButtonView}>
+                                 <Text onPress={() => { this.setState({ edit: true, firstname: this.state.eater.firstname, lastname: this.state.eater.lastname, eaterAlias: this.state.eater.eaterAlias }) } }
+                                     style={styleEaterPage.editButtonText}>Edit</Text>
+                             </View>
+                         </TouchableHighlight>
+                     </View>
+                     <Image source={chefProfile} style={styleEaterPage.eaterProfilePic}>
+                         <View style={styleEaterPage.uploadPhotoButtonView}>
+                             <TouchableHighlight onPress={() => this.uploadPic() }>
+                                 <Image source={require('./icons/ic_add_a_photo_48pt_3x.png') } style={styleEaterPage.iconImage}/>
+                             </TouchableHighlight>
+                         </View>
+                     </Image>
+                     <View style={styleEaterPage.eaterPageRowView}>
+                         <Text style={styleEaterPage.eaterNameText}>{this.state.eater.firstname} {this.state.eater.lastname} ({this.state.eater.eaterAlias}) </Text>
+                         <Text style={styleEaterPage.eaterPageGreyText}>{this.state.authProvider === 'Yumso' ? `Email:${this.state.eater.email}` : 'Logged in using Facebook'}</Text>
+                     </View>
+                     <View style={styleEaterPage.eaterPageRowView}>
+                         <Text style={styleEaterPage.eaterPageGreyText}>Address: </Text>
+                         {addressListRendered}
+                     </View>
+                     <View style={styleEaterPage.eaterPageRowView}>
+                         <Text style={styleEaterPage.eaterPageGreyText}>Payment: </Text>
+                         <Text style={styleEaterPage.eaterPageGreyText}>+ Credit Card: xxxx xxxx xxxx 1234</Text>
+                         <Text style={styleEaterPage.eaterPageGreyText}>+ Paypal: </Text>
+                     </View>
+                 </View>);
+         }
+     }
     
     uploadPic(){
            ImageCamera.PickImage((source)=>{
-               this.state.eater.eaterProfilePic = source.uri;
-               this.setState({eater:this.state.eater});
+                this.state.eater.eaterProfilePic = source.uri;
+                this.setState({eater:this.state.eater});
+                var photo = {
+                    uri: source.uri,
+                    type: 'image/jpeg',
+                    name: 'photo.jpg',
+                };
+                var request = new XMLHttpRequest();
+
+                var body = new FormData();
+                body.append('photo', photo);
+                body.append('title', 'A beautiful photo!');
+                
+                request.onreadystatechange = (e) => {
+                    if (request.readyState !== 4) {
+                        return;
+                    }
+
+                    if (request.status === 200) {
+                        console.log('success', request.responseText);
+                        Alert.alert( 'Success', 'Successfully upload your profile picture',[ { text: 'OK' }]); 
+                    } else {
+                        Alert.alert( 'Fail', 'Failed to upload your profile picture. Please retry again later',[ { text: 'OK' }]); 
+                    }
+                };  
+                
+                request.open('POST', config.baseUrl+config.eaterPicUploadEndpoint);
+                return AsyncStorage.getItem('token').then((token)=>{
+                    console.log(token);
+                    request.setRequestHeader("Authorization", 'bearer '+token);
+                    request.send(body);    
+                });
            });     
+    }
+    
+    submit(){
+        if (!this.state.firstname || this.state.firstname === '' || this.state.firstname.trim().includes(' ')) {
+            Alert.alert('Warning', 'Missing first name or first name is invalid', [{ text: 'OK' }]);    
+            return;            
+        }        
+        if (!this.state.lastname || this.state.lastname === '' || this.state.lastname.trim().includes(' ')) {
+            Alert.alert('Warning', 'Missing last name or last name is invalid', [{ text: 'OK' }]);                
+        }
+        if (!this.state.eaterAlias || this.state.eaterAlias === '') {
+            Alert.alert('Warning', 'Missing alias name. This will be displayed publicly to chef and other users', [{ text: 'OK' }]);                
+        }
+        var _this = this;
+        let eater = JSON.parse(JSON.stringify(this.state.eater));
+        eater.firstname = this.state.firstname.trim();
+        eater.lastname = this.state.lastname.trim();
+        eater.eaterAlias = this.state.eaterAlias.trim();
+        console.log(eater);
+        console.log(this.state.eater);
+        return this.client.postWithAuth(config.eaterUpdateEndpoint,{
+            eater: eater
+        }).then((res)=>{
+            if(res.statusCode ===200){
+                Alert.alert('Success', 'Successfully updated your profile', [{ text: 'OK' }]); 
+                _this.setState({eater:eater, edit:false});  
+                _this.state.callback(_this.state.eater);           
+            }else {
+                Alert.alert('Fail', 'Failed update your profile. Please retry again later', [{ text: 'OK' }]);          
+            }
+        });
     }
     
     navigateBackToChefList(){
