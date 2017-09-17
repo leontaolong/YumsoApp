f8378f4f82d6d322cba3250e5fa8f48e708f0c1b
leontaolong <longt8@uw.edu>
2017-09-17 13:12:10 -0700

    Sync unchanged file

--- YumsoApp/ios/YumsoApp/AppDelegate.m
+++ YumsoApp/ios/YumsoApp/AppDelegate.m
@@ -34,7 +34,7 @@
    * on the same Wi-Fi network.
    */
 
-  jsCodeLocation = [NSURL URLWithString:@"http://192.168.3.86:8081/index.ios.bundle?platform=ios&dev=true"];
+  jsCodeLocation = [NSURL URLWithString:@"http://10.104.200.111:8081/index.ios.bundle?platform=ios&dev=true"];
 
   /**
    * OPTION 2
